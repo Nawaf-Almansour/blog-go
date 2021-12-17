de9a5ebafe895df8ce8fce80f92decb1a9ce8a54
Nawaf Almansour <nawaaf.almansour@gmail.com>
2021-12-17 22:26:31 +0300

    added display movie

--- src/component/OneMovie.js
+++ src/component/OneMovie.js
@@ -31,23 +31,56 @@ export default function OneMovie() {
             });
     }
 
-    return (
-        <>
 
-            <h2>Movie: {movie.title} {movie.year}</h2>
-
-            <table className="table table-compact table-striped">
-                <thead></thead>
-                <tbody>
-                <tr>
-                    <td>
-                        <strong>Title:</strong>
-                    </td>
-                    <td>{movie.title}</td>
-                </tr>
-                </tbody>
-            </table>
-
-        </>
-    )
+    if (movie.genres) {
+        movie.genres = Object.values(movie.genres);
+    } else {
+        movie.genres = [];
+    }
+    if (error) {
+        return (<p>Error : {error.message}</p>)
+    } else if (!isLoaded) {
+        return (<p>Loading...</p>)
+    } else {
+        return (
+            <>
+                <h2>Movie: {movie.title} {movie.year}</h2>
+                <div className="float-start">
+                    <small>Rating: {movie.mpaa_rating}</small>
+                </div>
+                <div className="float-end">
+                    {movie.genres.map((m, index) => (
+                        <span className="badge bg-secondary me-1">
+                            {m}
+                        </span>
+                        ))}
+                </div>
+                <div className="clearfix"></div>
+                <hr/>
+                <table className="table table-compact table-striped">
+                    <thead></thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <strong>Title:</strong>
+                        </td>
+                        <td>{movie.title}</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>Description:</strong>
+                        </td>
+                        <td>{movie.description}</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>Run time:</strong>
+                        </td>
+                        <td>{movie.runtime} minutes</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </>
+        )
+    }
 }
